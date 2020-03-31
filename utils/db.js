54cd03fd907642f0284128ab0d5727ef53f6df61
lalawuhan <precious.segoe@gmail.com>
2020-03-31 18:07:08 +0200

    database, and new table. Add bcrypt file

---
+++ utils/db.js
@@ -0,0 +1,17 @@
+const spicedPg = require("spiced-pg");
+const db = spicedPg(
+    process.env.DATABASE_URL ||
+        "postgres:postgres:postgres@localhost:5432/socialnetwork"
+);
+
+// USERS
+//add a new user
+module.exports.addUser = (first, last, email, password) => {
+    const q = `
+        INSERT INTO users (first, last, email, password)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *
+    `;
+    const params = [first, last, email, password];
+    return db.query(q, params);
+};
