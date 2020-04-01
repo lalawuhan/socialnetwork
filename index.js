014ad15333bc290bd6d25991d012869b4f0ecbf8
lalawuhan <precious.segoe@gmail.com>
2020-04-01 23:19:35 +0200

    working routes post, need to fix if conditions

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const db = require("./utils/db");
 const { hash, compare } = require("./utils/bcrypt");
 const cookieSession = require("cookie-session");
 const csurf = require("csurf");
+const ses = require("./utils/ses");
+const cryptoRandomString = require("crypto-random-string");
 
 app.use(compression());
 
@@ -18,13 +20,13 @@ app.use(
         maxAge: 1000 * 60 * 60 * 24 * 7 * 6
     })
 );
-
+/* 
 app.use(csurf());
 
 app.use(function(req, res, next) {
     res.cookie("mytoken", req.csrfToken());
     next();
-});
+}); */
 
 if (process.env.NODE_ENV != "production") {
     app.use(
@@ -44,6 +46,8 @@ app.get("/welcome", (req, res) => {
         res.redirect("/");
     }
 });
+
+//register
 
 app.post("/register", (req, res) => {
     console.log("post /register body", req.body);
@@ -70,16 +74,18 @@ app.post("/register", (req, res) => {
         });
 });
 
+//login
+
 app.post("/login", (req, res) => {
     const { email, password } = req.body;
-    db.loginUser(email).then(result => {
+    db.getUser(email).then(result => {
         compare(password, result.rows[0].password)
             .then(matchValue => {
                 if (matchValue) {
                     req.session.userId = result.rows[0].id;
                     res.json({ success: true });
                 } else {
-                    console.log("error in compare ");
+                    console.log("error in compare passwords ");
                     res.json({ success: false });
                 }
             })
@@ -88,6 +94,68 @@ app.post("/login", (req, res) => {
                 res.json({ success: false });
             });
     });
+});
+
+//forgot password
+//1 : POST /password/reset/start
+app.post("/password/reset/start", (req, res) => {
+    //const email = req.body.email;
+    const secretCode = cryptoRandomString({
+        length: 6
+    });
+    console.log("post request to /reset happened");
+    db.getUser(req.body.email).then(result => {
+        console.log("user exists", result.rows[0]);
+        if (result.rows[0] != undefined) {
+            //generate secret code
+
+            console.log("secret code is", secretCode);
+            db.generateResetCode(req.body.email, secretCode);
+            ses.sendEmail(
+                "users-email@gmail.com",
+                "Yout reset code",
+                "a super secret code " + secretCode
+            );
+            res.json({ success: true });
+        } else {
+            console.log(" error in (getUser index.js)");
+        }
+    });
+});
+
+//2: POST /password/reset/verify
+app.post("/password/reset/verify", (req, res) => {
+    console.log("reset verify body", req.body);
+    //const { email, password } = req.body;
+    db.verifySecretCode(req.body.email)
+        .then(result => {
+            console.log("result from verify ", result.rows);
+            if (result.rows != undefined) {
+                hash(req.body.password)
+                    .then(hashedPw => {
+                        db.updatePassword(req.body.email, hashedPw)
+                            .then(res.json({ success: true }))
+                            .catch(error => {
+                                console.log(
+                                    "error in updating password /verify ",
+                                    error
+                                );
+                                res.json({ success: false });
+                            });
+                    })
+                    .catch(error => {
+                        console.log("error in post /verify", error);
+                        res.json({ success: false });
+                    });
+            } else {
+                console.log("did not verify code  in /password/reset/verify");
+                res.json({ success: false });
+            }
+        })
+        .catch(error => {
+            console.log("error on whole reset/ db bverify secret code", error);
+            res.json({ success: false });
+        });
 });
 
 //this route needs to be the last route
@@ -106,3 +174,4 @@ app.listen(8080, function() {
 
 //lsof -i tcp:8080
 //kill -9
+//global.catsup@spicedling.email
