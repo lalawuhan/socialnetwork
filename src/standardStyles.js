4cc07c2cf117cc7c2f7859edb5fff0dd6d27af2b
lalawuhan <precious.segoe@gmail.com>
2020-04-03 19:53:28 +0200

    added styled components, basic settings

---
+++ src/standardStyles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+`;
+export const Navbar = styled.div`
+    padding: 2em;
+    background: papayawhip;
+`;
+
+export const UpdateWrap = styled.div`
+    padding: 1em 0;
+    background-color: azure;
+    display: flex;
+    flex-direction: column;
+`;
+export const ProfileWrapper = styled.div`
+    padding: 1em 0;
+    background-color: azure;
+    display: flex;
+    justify-content: space-around;
+`;
