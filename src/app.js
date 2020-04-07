8015ec36eff01a3c0831df156fff21a46dcde5e4
lalawuhan <precious.segoe@gmail.com>
2020-04-07 17:04:51 +0200

    refactor app.js to use hooks

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import { BrowserRouter, Route } from "react-router-dom";
 import ProfilePic from "./profilepic";
@@ -8,87 +8,83 @@ import OtherProfile from "./otherProfile";
 import FindPeople from "./findpeople";
 import { Navbar } from "./standardStyles.js";
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    componentDidMount() {
-        var self = this;
+export default function App() {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
         axios
             .get("/user")
             .then(function (response) {
-                self.setState(response.data);
-                console.log("state from app", self.state);
+                setData(response.data);
             })
             .catch(function (error) {
                 console.log("error in get(/user) app.js", error);
             });
-    }
-    toggleModal() {
-        this.setState({
-            uploaderVisibility: !this.state.uploaderVisibility,
-        });
-    }
+    }, []);
 
-    render() {
-        return (
-            <React.Fragment>
-                <BrowserRouter>
-                    <Navbar className="navbar">
-                        <ProfilePic
-                            first={this.state.first}
-                            last={this.state.last}
-                            image_url={this.state.image_url}
-                            toggleModal={() => this.toggleModal()}
-                        />
-                        <h1>
-                            {this.state.first} {this.state.last}
-                        </h1>
-                        <p>{this.state.biography}</p>
-                    </Navbar>
-                    {this.state.uploaderVisibility && (
-                        <Uploader
-                            changeImageUrl={(image_url) =>
-                                this.setState({
-                                    image_url: image_url,
-                                    uploaderVisibility: false,
+    return (
+        <React.Fragment>
+            <BrowserRouter>
+                <Navbar className="navbar">
+                    <ProfilePic
+                        first={data.first}
+                        last={data.last}
+                        image_url={data.image_url}
+                        toggleModal={() =>
+                            setData({
+                                ...data,
+                                uploaderVisibility: !data.uploaderVisibility,
+                            })
+                        }
+                    />
+                    <h1>
+                        {data.first} {data.last}
+                    </h1>
+                    <p>{data.biography}</p>
+                </Navbar>
+                {data.uploaderVisibility && (
+                    <Uploader
+                        changeImageUrl={(image_url) =>
+                            setData({
+                                ...data,
+                                image_url: image_url,
+                                uploaderVisibility: false,
+                            })
+                        }
+                    />
+                )}
+                <Route
+                    exact
+                    path="/"
+                    render={() => (
+                        <Profile
+                            first={data.first}
+                            last={data.last}
+                            image_url={data.image_url}
+                            addBio={data.biography}
+                            setBio={(addBio) =>
+                                setData({
+                                    ...data,
+                                    biography: addBio,
                                 })
                             }
                         />
                     )}
-                    <Route
-                        exact
-                        path="/"
-                        render={() => (
-                            <Profile
-                                first={this.state.first}
-                                last={this.state.last}
-                                image_url={this.state.image_url}
-                                addBio={this.state.biography}
-                                setBio={(addBio) =>
-                                    this.setState({
-                                        biography: addBio,
-                                    })
-                                }
-                            />
-                        )}
-                    />
+                />
 
-                    {/* force a new component to be rendered */}
-                    <Route
-                        path="/user/:id"
-                        render={(props) => (
-                            <OtherProfile
-                                key={props.match.url}
-                                match={props.match}
-                                history={props.history}
-                            />
-                        )}
-                    />
-                    <Route path="/users" component={FindPeople} />
-                </BrowserRouter>
-            </React.Fragment>
-        );
-    }
+                {/* force a new component to be rendered */}
+                <Route
+                    path="/user/:id"
+                    render={(props) => (
+                        <OtherProfile
+                            key={props.match.url}
+                            match={props.match}
+                            history={props.history}
+                        />
+                    )}
+                />
+                <Route path="/users" component={FindPeople} />
+            </BrowserRouter>
+        </React.Fragment>
+    );
 }
