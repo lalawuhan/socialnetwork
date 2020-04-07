2b9ca0c9ac9d4ad0c3da128d06975bb27de35f13
lalawuhan <precious.segoe@gmail.com>
2020-04-07 14:54:16 +0200

    -new findpeople component -add axios req for 3 recent users -users rendered on screen -add input field to search for users -added [] to clean up results -store search results in state

---
+++ src/findpeople.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import { Link } from "react-router-dom";
+
+export default function FindPeople() {
+    const [newestUsers, setNewestUsers] = useState([]); //initial users from list : array destructuring
+    const [searchResults, setSearchResults] = useState();
+    const handleChange = (e) => {
+        setSearchResults(e.target.value);
+    };
+
+    useEffect(() => {
+        axios
+            .get("/newestUsers")
+            .then(({ data }) => {
+                //console.log("findpeople.js GET newestusers: ", data);
+                setNewestUsers(data);
+            })
+            .catch((error) => {
+                console.log("findpeople,js GET newestusers error ", error);
+            });
+    }, []);
+
+    useEffect(() => {
+        if (!searchResults) {
+            return;
+        }
+        axios
+            .get("/searchUsers/?q=" + searchResults)
+            .then(({ data }) => {
+                setNewestUsers(data);
+            })
+            .catch((error) => {
+                console.log(
+                    "findpeople.js catch error in search results",
+                    error
+                );
+            });
+    }, [searchResults]); //useEffect's dependency array
+
+    return (
+        <div>
+            <>
+                <input
+                    id="search-users"
+                    type="text"
+                    placeholder="Search for a user"
+                    onChange={handleChange}
+                />
+                {searchResults == undefined && <h2>New Users:</h2>}
+            </>
+
+            <div>
+                {newestUsers.map((user) => (
+                    <div className="user-search-link" key={user.id}>
+                        <Link to={"/user/" + user.id}>
+                            <h3>
+                                {user.first} {user.last}
+                            </h3>
+                            <img
+                                src={user.image_url || "/images/default.png"}
+                            />
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
