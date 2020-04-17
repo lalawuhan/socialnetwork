14740761856303b0721e7552758765bd6e3ebc1c
lalawuhan <precious.segoe@gmail.com>
2020-04-17 21:47:09 +0200

    add modal to upload img v1

--- src/uploader.js
+++ src/uploader.js
@@ -1,51 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "./axios";
+import Modal from "react-modal";
+import { Button } from "./standardStyles.js";
 
-export default class Uploader extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    componentDidMount() {
-        console.log("uploader mountd");
-    }
-    handleChange(e) {
-        this.setState({
-            file: e.target.files[0]
+export default function Uploader(props) {
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    const [data, setData] = useState({});
+
+    const handleChange = (e) => {
+        setData({
+            file: e.target.files[0],
         });
+    };
+    Modal.setAppElement("#main");
+
+    function openModal() {
+        setIsOpen(true);
     }
-    uploadImage() {
-        console.log("I am uploading image now");
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    const customStyles = {
+        content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            padding: "2em",
+            background: "#121212",
+            color: "white",
+            width: "25em",
+        },
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
         var formData = new FormData();
-        var self = this;
-        formData.append("file", this.state.file);
+        formData.append("file", data.file);
         axios
             .post("/upload", formData)
-            .then(function(response) {
+            .then(function (response) {
                 console.log(
                     "uploaded image response:",
                     response.data.rows[0].image_url
                 );
-                self.props.changeImageUrl(response.data.rows[0].image_url); //this.props.muffin("pass an argument back like url")
+                props.changeImageUrl(response.data.rows[0].image_url);
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.log("error in POST /upload uploader.js", error);
             });
-    }
-    render() {
-        return (
-            <>
-                <h3>Upload New Profile Photo</h3>
-                <input
-                    type="file"
-                    name="file"
-                    id="file"
-                    onChange={e => this.handleChange(e)}
-                />
-                <button onClick={() => this.uploadImage()}>
-                    Submit image{" "}
-                </button>
-            </>
-        );
-    }
+    };
+    return (
+        <div>
+            <div>
+                <Button onClick={openModal}>Upload Photo</Button>
+                <Modal
+                    style={customStyles}
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Delete Account Modal"
+                    closeTimeoutMS={1000}
+                >
+                    <h3>Upload New Profile Photo</h3>
+                    <input
+                        type="file"
+                        name="file"
+                        id="file"
+                        onChange={handleChange}
+                    />
+                    <Button success onClick={handleSubmit}>
+                        Submit image{" "}
+                    </Button>
+                    <Button onClick={closeModal} primary>
+                        Cancel
+                    </Button>
+                </Modal>
+            </div>
+        </div>
+    );
 }
